0b025b4b057a73c5e3382277886d1fa2332ecfcb
Susana Velasco <svelped076@g.educaand.es>
2024-04-26 18:31:20 +0200

    añadiendo als erver.js Content-Security-Policy

--- divertidafront/server.js
+++ divertidafront/server.js
@@ -5,6 +5,12 @@ const app = express();
 // Serve static files
 app.use(express.static(`${__dirname}/divertidafront/dist`));
 
+// Content Security Policy configuration
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src 'self' 'unsafe-inline'");
+  next();
+});
+
 // Send all requests to index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'divertidafront', 'dist', 'index.html'));
