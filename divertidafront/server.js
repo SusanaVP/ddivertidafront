28bafa48315c7424b8af332d0912f449c13f7a7a
Susana Velasco <svelped076@g.educaand.es>
2024-04-26 18:21:24 +0200

    sigo con cambios en el server.js

--- divertidafront/server.js
+++ divertidafront/server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/divertidafront'));
+app.use(express.static('${__dirname}/divertidafront/dist)'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/divertidafront/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile('./divertidafront/dist/index.html');
 });
 
 // default Heroku port
